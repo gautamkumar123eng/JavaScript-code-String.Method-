a4c5aaed5fbbe3204fc5cf8eb77a3ee3a6303e8f
Gautam Kumar <91307706+gautamkumar123eng@users.noreply.github.com>
2023-04-10 03:07:19 +0530

    Update string.replace().js

--- string.replace().js
+++ string.replace().js
@@ -1,2 +1,6 @@
 let text="gautam, rupesh,dheeraj";
 console.log(text.replace("dheeraj","Neeraj"));
+
+// replace all matches, use a regular expression with [a /g]flag
+//let text="gautam, rupesh,dheeraj and dheeraj";
+//console.log(text.replace(/dheeraj/g,"Neeraj"));
