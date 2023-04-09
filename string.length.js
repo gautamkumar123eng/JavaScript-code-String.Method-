0e731abaffbc644c03f0316f4b34dedec0bcece9
Gautam Kumar <91307706+gautamkumar123eng@users.noreply.github.com>
2023-04-10 02:12:02 +0530

    The length property returns the length of a string

---
+++ string.length.js
@@ -0,0 +1,12 @@
+//The length property returns the length of a string:
+//1st type-------
+// let text ="Gautam";
+// let x =text.length
+// console.log(x);
+
+//2nd type-------------
+//console.log("gautam".length)
+
+//3rd type------------
+let text ="Gautam";
+console.log(text.length);
